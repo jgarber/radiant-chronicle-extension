eba9227c3a669bff820f7928577db15369c0588e
Jason Garber <jg@jasongarber.com>
2009-03-25 13:09:54 -0400

    Refactor specs

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -20,37 +20,10 @@ describe Page do
     
     lambda { 
       @page.save.should == true
-    }.should change{ @page.versions.length }.by(1)
+    }.should create_new_version
     
     @page.versions.current.model.should == @page
     
-  end
-  
-  it "should not change the live version when Page is updated as a draft" do
-    @page = pages(:first)
-    @page.status_id = Status[:draft].id
-    
-    lambda { 
-      @page.save.should == true
-    }.should change{ @page.versions.length }.by(1)
-    
-    
-    @page.reload
-    @page.status_id.should_not == Status[:draft].id
-  end
-  
-  it "should properly save a version when Page is updated as a draft" do
-    @page = pages(:first)
-    @page.title = "This is just a draft"
-    @page.status_id = Status[:draft].id
-    
-    lambda { 
-      @page.save.should == true
-    }.should change{ @page.versions.length }.by(1)
-    
-    @page.reload
-    @page.versions.current.model.title.should == "This is just a draft"
-    @page.versions.current.model.status_id.should == Status[:draft].id
   end
   
   it "should create a new draft in the main table" do
@@ -59,32 +32,60 @@ describe Page do
     @page.status.should == Status[:draft]
   end
   
-  it "should not change the live version when PagePart is updated as a draft" do
-    @page = pages(:first)
-    @page.status_id = Status[:draft].id
-    @page.parts = [{"name"=>"body", "filter_id"=>"", "content"=>"I changed the body!"}]
+  describe "drafts" do
+    before(:each) do
+      @page = pages(:first)
+      @page.status_id = Status[:draft].id
+    end
     
-    lambda { 
-      @page.save.should == true
-    }.should change{ @page.versions.length }.by(1)
+    it "should not change the live version when Page is updated as a draft" do
+      lambda { 
+        @page.save.should == true
+      }.should create_new_version
     
-    @page.reload
-    @page.status_id.should_not == Status[:draft].id
-    @page.parts.first.content.should_not == "I changed the body!"
-  end
+    
+      @page.reload
+      @page.status_id.should_not == Status[:draft].id
+    end
   
-  it "should properly save a version when PagePart is updated as a draft" do
-    @page = pages(:first)
-    @page.parts = [{"name"=>"body", "filter_id"=>"", "content"=>"I changed the body!"}]
-    @page.status_id = Status[:draft].id
+    it "should properly save a version when Page is updated as a draft" do
+      @page.title = "This is just a draft"
     
-    lambda { 
-      @page.save.should == true
-    }.should change{ @page.versions.length }.by(1)
+      lambda { 
+        @page.save.should == true
+      }.should create_new_version
     
-    @page.reload
-    @page.versions.current.model.parts.first.content.should == "I changed the body!"
-    @page.versions.current.model.status_id.should == Status[:draft].id
+      @page.reload
+      @page.versions.current.model.title.should == "This is just a draft"
+      @page.versions.current.model.status_id.should == Status[:draft].id
+    end
+  
+    it "should not change the live version when PagePart is updated as a draft" do
+      @page.parts = [{"name"=>"body", "filter_id"=>"", "content"=>"I changed the body!"}]
+    
+      lambda { 
+        @page.save.should == true
+      }.should create_new_version
+    
+      @page.reload
+      @page.status_id.should_not == Status[:draft].id
+      @page.parts.first.content.should_not == "I changed the body!"
+    end
+  
+    it "should properly save a version when PagePart is updated as a draft" do
+      @page.parts = [{"name"=>"body", "filter_id"=>"", "content"=>"I changed the body!"}]
+    
+      lambda { 
+        @page.save.should == true
+      }.should create_new_version
+    
+      @page.reload
+      @page.versions.current.model.parts.first.content.should == "I changed the body!"
+      @page.versions.current.model.status_id.should == Status[:draft].id
+    end
   end
 
+  def create_new_version
+    change{ @page.versions.length }.by(1)
+  end
 end
