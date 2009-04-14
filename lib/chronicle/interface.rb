422474621a964fd52c81b53d3cb29eb426a0206c
Jason Garber <jg@jasongarber.com>
2009-04-14 16:32:22 -0400

    Added HelpBalloon.js to show version details (just a proof-of-concept for now).

--- lib/chronicle/interface.rb
+++ lib/chronicle/interface.rb
@@ -1,14 +1,15 @@
 module Chronicle::Interface
   def self.included(base)
     base.class_eval {
-      before_filter :add_chronicle_stylesheet, :only => [:edit, :new]
+      before_filter :add_chronicle_assets, :only => [:edit, :new]
       include InstanceMethods
     }
   end
   
   module InstanceMethods
-    def add_chronicle_stylesheet
+    def add_chronicle_assets
       include_stylesheet 'admin/chronicle'
+      include_javascript 'admin/HelpBalloon'
     end
   end
 end
