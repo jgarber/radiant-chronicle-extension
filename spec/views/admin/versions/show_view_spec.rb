c0ea304cca06c4684d11d32e903ac71a1242bdb7
Jason Garber <jg@jasongarber.com>
2009-04-22 14:07:02 -0400

    Changed version time display to time ago in words

--- spec/views/admin/versions/show_view_spec.rb
+++ spec/views/admin/versions/show_view_spec.rb
@@ -19,7 +19,7 @@ describe "/admin/versions/show" do
   describe "second version" do
     before do
       @version = pages(:updated_by_existing).versions.current
-      @version.instance.stub!(:updated_at).and_return Time.local(2009,1,1,8,57)
+      @version.instance.stub!(:updated_at).and_return 1.hour.ago
       assigns[:version] = @version
       render 'admin/versions/show'
     end 
@@ -32,12 +32,25 @@ describe "/admin/versions/show" do
       response.should have_selector("span.version-author", :content => "Existing")
     end
   
-    it "should display the update time" do
-      response.should have_selector("span.version-updated-at", :content => "01 Jan 08:57")
+    it "should display the update time in abbreviated form" do
+      response.should have_selector("span.version-updated-at") do |span|
+        span.should have_selector("abbr", :content => "about 1 hour ago")
+      end
     end
   
     it "should display the status" do
       response.should have_selector("span.version-status", :content => "Published")
     end
-  end  
+  end
+  
+  it "should display the update time in extended form in abbr title" do
+    @version = pages(:updated_by_existing).versions.current
+    @version.instance.stub!(:updated_at).and_return Time.local(2009,1,1,8,57)
+    assigns[:version] = @version
+    render 'admin/versions/show'
+    
+    response.should have_selector("span.version-updated-at") do |span|
+      span.should have_selector("abbr", :title => "January 01, 2009 08:57")
+    end
+  end
 end
