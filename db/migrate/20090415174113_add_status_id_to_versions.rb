f9f97a3cb1ef3f6bad088ca1ff0c7a332b45cc68
Jason Garber <jg@jasongarber.com>
2009-04-22 10:53:30 -0400

    Populate the status_id column on migration.

--- db/migrate/20090415174113_add_status_id_to_versions.rb
+++ db/migrate/20090415174113_add_status_id_to_versions.rb
@@ -1,6 +1,12 @@
 class AddStatusIdToVersions < ActiveRecord::Migration
   def self.up
     add_column :versions, :status_id, :integer
+    
+    Version.reset_column_information 
+    Version.all.each do |version|
+      version.status_id = version.instance.status_id
+      version.save
+    end
   end
 
   def self.down
