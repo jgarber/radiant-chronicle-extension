0cf1ea3e6009c80a381655c01b03e01b0d8d5336
Sean Cribbs <seancribbs@gmail.com>
2009-07-18 03:22:50 +0800

    Remove deprecation warning.
    Signed-off-by: Jason Garber <jg@jasongarber.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../../../../config/environment')
 
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
+require 'cucumber/formatter/unicode' # Comment out this line if you don't want Cucumber Unicode support
 require 'webrat'
  
 Webrat.configure do |config|
