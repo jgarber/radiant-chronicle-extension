0b97ec906b23f61a2ed1787d4ace265141e70d02
Jason Garber <jg@jasongarber.com>
2009-09-21 14:56:53 -0400

    Combining the migrations from c8a0339 and e518c3 so it might actually convert properly this time without creating page parts that duplicate themselves when edited because they're missing ids.

--- db/migrate/20090626143038_convert_versioned_association_from_hash_to_object.rb
+++ db/migrate/20090626143038_convert_versioned_association_from_hash_to_object.rb
@@ -4,7 +4,13 @@ class ConvertVersionedAssociationFromHashToObject < ActiveRecord::Migration
       attributes = YAML::load(version.yaml)
       if attributes["parts"]
         attributes["parts"].collect! do |part_attributes|
-          PagePart.send(:instantiate, part_attributes)
+          part_attributes.delete("page_id")
+          if existing_part = PagePart.find_by_page_id_and_name(version.versionable_id, part_attributes["name"])
+              existing_part.attributes = part_attributes
+              existing_part
+          else
+            PagePart.send(:instantiate, part_attributes)
+          end
         end
         version.update_attributes( :yaml => attributes.to_yaml )
       end
