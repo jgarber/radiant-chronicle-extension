7634e61a679fef974b1d1f1f5e214800a7393a79
Jason Garber <jg@jasongarber.com>
2009-09-21 15:55:27 -0400

    Try that migration one more time.  You see, before when you created a draft and it remained a draft through the current version, it had no page parts in the main table.  Now, it does, so if it's the current version but has no page parts in the main table, create them.
    Let's hope this is the last time I have to change this file!

--- db/migrate/20090626143038_convert_versioned_association_from_hash_to_object.rb
+++ db/migrate/20090626143038_convert_versioned_association_from_hash_to_object.rb
@@ -4,12 +4,17 @@ class ConvertVersionedAssociationFromHashToObject < ActiveRecord::Migration
       attributes = YAML::load(version.yaml)
       if attributes["parts"]
         attributes["parts"].collect! do |part_attributes|
-          part_attributes.delete("page_id")
           if existing_part = PagePart.find_by_page_id_and_name(version.versionable_id, part_attributes["name"])
-              existing_part.attributes = part_attributes
-              existing_part
+            part_attributes.delete("page_id")
+            existing_part.attributes = part_attributes
+            existing_part
           else
-            PagePart.send(:instantiate, part_attributes)
+            part_attributes["page_id"] = version.versionable_id
+            if version.next == nil
+              PagePart.create(part_attributes)
+            else
+              PagePart.send(:instantiate, part_attributes)
+            end
           end
         end
         version.update_attributes( :yaml => attributes.to_yaml )
